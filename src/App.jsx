68335819ede4ef255e093295290076340306fb40
AhmedSaeed216 <ahmed.saeed6420@gmail.com>
2024-10-29 12:01:35 +0500

    setup uploaded

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+//   const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <h1 className=' font-bold text-red-700'>hi</h1>
+      <h2 className=''></h2>
+    </>
+  )
+}
+
+export default App
